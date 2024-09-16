aac59f3b1aaa9b8e489d74e6f99e9c6b1e400ae5
Sreelakshmy-S <163990159+Sreelakshmy-S@users.noreply.github.com>
2024-09-16 16:22:18 +0530

    Update Reverse_String.py

--- Reverse_String.py
+++ Reverse_String.py
@@ -1,6 +1,10 @@
 # Daily Programming Challenge 2024 : Day 8
 # Reverse a String Word by Word
-
+'''
+You are given a string s that consists of multiple words separated by spaces. 
+Your task is to reverse the order of the words in the string. Words are defined as sequences of non-space characters. 
+The output string should not contain leading or trailing spaces, and multiple spaces between words should be reduced to a single space.
+'''
 string = input()
 string = string.strip('""')
 sentence = []                   # List stores the words in order
